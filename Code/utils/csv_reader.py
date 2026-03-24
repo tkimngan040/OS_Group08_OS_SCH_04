4ded537338cae4f733dd25ea13d074129b5d4ce6
quanvh3068-hue <quanvh3068@ut.edu.vn>
2026-03-24 19:56:23 +0700

    add csv_reader.py

---
+++ Code/utils/csv_reader.py
@@ -0,0 +1,23 @@
+import csv
+from process import Process
+
+def read_csv(file_name):
+    processes = []
+
+    try:
+        with open(file_name, mode='r') as file:
+            reader = csv.DictReader(file)
+
+            for row in reader:
+                p = Process(
+                    pid=row['pid'],
+                    arrival_time=int(row['arrival_time']),
+                    burst_time=int(row['burst_time']),
+                    priority=int(row['priority'])
+                )
+                processes.append(p)
+
+    except FileNotFoundError:
+        print("Không tìm thấy file")
+
+    return processes
