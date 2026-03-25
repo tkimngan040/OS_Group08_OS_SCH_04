6d7c3e92afbf7048d1a9e220187feb4048d3be05
nguyenxbao09-creator <nguyenxbao09@gmail.com>
2026-03-25 19:52:39 +0700

    Update process.py

--- Code/models/process.py
+++ Code/models/process.py
@@ -4,12 +4,12 @@ import os
 class Process:
     def __init__(self, pid, arrival_time, burst_time, priority=0):
         # Thông tin đầu vào từ file CSV
-        self.pid = pid                # ID tiến trình (P1, P2...)
-        self.arrival_time = arrival_time  # Thời điểm đến
-        self.burst_time = burst_time      # Thời gian chạy thực tế
-        self.priority = priority          # Độ ưu tiên
+        self.pid = pid
+        self.arrival_time = arrival_time
+        self.burst_time = burst_time
+        self.priority = priority
 
-        # Các thông số tính toán (Thành viên khác sẽ dùng cái này)
+        # Các thông số tính toán
         self.start_time = 0
         self.completion_time = 0
         self.waiting_time = 0
@@ -17,3 +17,33 @@ class Process:
 
     def __repr__(self):
         return f"Process({self.pid}, AT={self.arrival_time}, BT={self.burst_time})"
+
+
+
+def calculate_times(processes):
+    # Sắp xếp theo arrival time (FCFS)
+    processes.sort(key=lambda x: x.arrival_time)
+
+    current_time = 0
+
+    for p in processes:
+        
+        if current_time < p.arrival_time:
+            current_time = p.arrival_time
+
+        # Thời điểm bắt đầu
+        p.start_time = current_time
+
+        # Thời điểm hoàn thành
+        p.completion_time = p.start_time + p.burst_time
+
+        # Turnaround time = CT - AT
+        p.turnaround_time = p.completion_time - p.arrival_time
+
+        # Waiting time = TAT - BT
+        p.waiting_time = p.turnaround_time - p.burst_time
+
+        # Cập nhật thời gian hiện tại
+        current_time = p.completion_time
+
+    return processes
