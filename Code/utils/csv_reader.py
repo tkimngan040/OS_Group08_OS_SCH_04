b2c5f811583cb5c4c3328a1f28b6bc18ce41b599
quanvh3068-hue <quanvh3068@ut.edu.vn>
2026-03-26 09:40:32 +0700

    Update csv_reader.py

--- Code/utils/csv_reader.py
+++ Code/utils/csv_reader.py
@@ -1,23 +1,22 @@
-import csv
-from models.process import Process
+import pandas as pd
+import os
+from utils.process import Process
 
-def read_csv(file_name):
-    processes = []
-
-    try:
-        with open(file_name, mode='r') as file:
-            reader = csv.DictReader(file)
-
-            for row in reader:
-                p = Process(
-                    pid=row['pid'],
-                    arrival_time=int(row['arrival_time']),
-                    burst_time=int(row['burst_time']),
-                    priority=int(row['priority'])
-                )
-                processes.append(p)
-
-    except FileNotFoundError:
+def read_csv(file_path):
+    if not os.path.exists(file_path):
         print("Không tìm thấy file")
+        return []
+
+    df = pd.read_csv(file_path)
+
+    processes = []
+    for _, row in df.iterrows():
+        p = Process(
+            row['pid'],
+            row['arrival_time'],
+            row['burst_time'],
+            row.get('priority', 0)
+        )
+        processes.append(p)
 
     return processes
