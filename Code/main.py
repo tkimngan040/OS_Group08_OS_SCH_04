1ff9f8057ab01c6ebd87a5ac7e414bd78ca66b89
XuanHuan-solodevGodot <tranxuanhuan26@gmail.com>
2026-04-01 13:56:19 +0700

    Integrate process management and CSV export
    Added process handling and CSV export functionality.

--- Code/main.py
+++ Code/main.py
@@ -4,3 +4,11 @@ import tkinter as tk
 root = tk.Tk()
 app = App(root)
 root.mainloop()
+#phan nay de tao ra file result
+from process import Process
+from sjf import sjf_non_preemptive
+from csv_exporter import export_to_csv
+
+processes = [Process("P1", 0, 6), Process("P2", 1, 4), Process("P3", 2, 2)]
+order, result = sjf_non_preemptive(processes)
+export_to_csv(result)
